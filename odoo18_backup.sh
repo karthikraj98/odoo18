55793d1261fb9f55d6375eb97d622dfa05d08001
Bayanna Karthik <126453814+karthikraj98@users.noreply.github.com>
2025-05-29 13:18:28 +0530

    Update odoo18_backup.sh

--- odoo18_backup.sh
+++ odoo18_backup.sh
@@ -10,12 +10,8 @@ BACKUP_FILE="$BACKUP_DIR/${DB_NAME}_$DATE.dump"
 # Create backup directory if it doesn't exist
 mkdir -p "$BACKUP_DIR"
 
-# Dump the database without compression
-pg_dump -U "$DB_USER" -p 5432 -U odoo18 -h 127.0.0.1 -Fc -v -f "$BACKUP_FILE" "$DB_NAME"
+# Dump the database
+pg_dump -U "$DB_USER" -p 5432 -h 127.0.0.1 -Fc -v -f "$BACKUP_FILE" "$DB_NAME"
 
-# Optional: Delete backups older than 7 days
-find "$BACKUP_DIR" -type f -name "${DB_NAME}_*.sql" -mtime +7 -exec rm {} \;
-
-sudo /var/lib/jenkins/workspace/job2
-sudo chmod +x odoo18_backup.sh 
-sudo ./odoo18_backup.sh
+# Delete backups older than 7 days
+find "$BACKUP_DIR" -type f -name "${DB_NAME}_*.dump" -mtime +7 -exec rm {} \;
