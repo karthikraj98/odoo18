9a81a8b8a99abd76ff1528081379b2b3feb7a66d
Bayanna Karthik <126453814+karthikraj98@users.noreply.github.com>
2025-05-29 13:48:26 +0530

    Update odoo18_backup.sh

--- odoo18_backup.sh
+++ odoo18_backup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Variables
-BACKUP_DIR="/var/lib/jenkins/odoo_backups_1"
-DB_NAME="odoo18"
-DB_USER="odoo18"
-DATE=$(date +%Y-%m-%d_%H-%M-%S)
-BACKUP_FILE="$BACKUP_DIR/${DB_NAME}_$DATE.dump"
+CURRENT_IP=$(hostname -I | awk '{print $1}')
+CONFIG_FILE="/path/to/hosts.csv"
 
-# Create backup directory if it doesn't exist
-mkdir -p "$BACKUP_DIR"
-
-# Dump the database
-pg_dump -U "$DB_USER" -p 5432 -h 127.0.0.1 -Fc -v -f "$BACKUP_FILE" "$DB_NAME"
-
-# Delete backups older than 7 days
-find "$BACKUP_DIR" -type f -name "${DB_NAME}_*.dump" -mtime +7 -exec rm {} \;
+IFS=, # set delimiter
+tail -n +2 "$CONFIG_FILE" | while read ip db user; do
+    if [[ "$ip" == "$CURRENT_IP" ]]; then
+        DB_NAME="$db"
+        DB_USER="$user"
+        echo "Matched IP: $ip → $DB_NAME / $DB_USER"
+        break
+    fi
+done
